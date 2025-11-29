2bc48506f06eab77b02b8ef89863f799bcfe3428
WARP Cleanup Agency <sangreousx@gmail.com>
2025-11-29 12:10:55 +0100

    Update src.lua

--- src.lua
+++ src.lua
@@ -904,9 +904,7 @@ function Library:create(options)
 
 	rawset(mt, "creditsContainer", creditsTab.container)
 
-	creditsTab:credit{Name = "Abstract", Description = "UI Library Developer", Discord = "Abstract#8007", V3rmillion = "AbstractPoo"}
-	creditsTab:credit{Name = "Deity", Description = "UI Library Developer", Discord = "Deity#0228", V3rmillion = "0xDEITY"}
-	creditsTab:credit{Name = "Repository", Description = "UI Library Repository", Github="https://github.com/deeeity/mercury-lib/blob/master/src.lua"}
+	creditsTab:credit{Name = "Turkish Roach", Description = "UI Creator", Discord = "darillus_evil_twin"}
 
 	return mt
 end
